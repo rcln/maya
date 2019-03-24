29b2fbe8e31defaa17b83780bd20456963e52611
Heba kaddouh <38936332+Hebakaddouh@users.noreply.github.com>
2019-03-24 19:11:48 +0100

    Add files via upload

--- Front-end/src/components/User/SignIn.vue
+++ Front-end/src/components/User/SignIn.vue
@@ -1,1 +1,60 @@
+<template>
+  <v-container fluid fill-width color="white">
+    <p>Sign in page</p>
+      <form>
+      <v-text-field
+        v-model="email"
+        v-validate="'required|email'"
+        :error-messages="errors.collect('email')"
+        label="E-mail"
+        data-vv-name="email"
+        required
+      ></v-text-field>
+      <v-text-field
+        v-model="pwd"
+        v-validate="'required|pwd'"
+        :items="items"
+        :error-messages="errors.collect('pwd')"
+        label="Password"
+        data-vv-name="pwd"
+        type="password"
+        required
+      ></v-text-field>
+      <v-btn @click="submit">submit</v-btn>
+    </form>
+  </v-container>
+</template>
 
+<script>
+  import Vue from 'vue'
+  import VeeValidate from 'vee-validate'
+
+  Vue.use(VeeValidate)
+
+  export default {
+    $_veeValidate: {
+      validator: 'new'
+    },
+
+    data: () => ({
+      email: '',
+      pwd: '',
+      dictionary: {
+        attributes: {
+          email: 'E-mail Address'
+          // custom attributes
+        },
+      }
+    }),
+
+    mounted () {
+      this.$validator.localize('en', this.dictionary)
+    },
+
+    methods: {
+      submit () {
+        this.$validator.validateAll()
+      },
+    }
+  }
+</script>
---
+++ Front-end/src/components/User/SignUp.vue
@@ -0,0 +1,76 @@
+<template>
+  <v-container fluid fill-width color="white">
+    <v-layout row>
+      <v-flex xs12 sm6 offset-sm3>
+        <v-card>
+          <v-card-text>
+            <v-container>
+              <form>
+                <v-layout row>
+                  <v-flex xs12>
+                    <v-text-field
+                      name="email"
+                      label="Mail"
+                      id="email"
+                      v-model="email"
+                      type="email"
+                      required></v-text-field>
+                  </v-flex>
+                </v-layout>
+                <v-layout row>
+                  <v-flex xs12>
+                    <v-text-field
+                      name="password"
+                      label="Password"
+                      id="password"
+                      v-model="password"
+                      type="password"
+                      required></v-text-field>
+                  </v-flex>
+                </v-layout>
+                <v-layout row>
+                  <v-flex xs12>
+                    <v-text-field
+                      name="ConfirmPassword"
+                      label="Confirm Password"
+                      id="ConfirmPassword"
+                      v-model="ConfirmPassword"
+                      type="password"
+                      :rules="[comparePasswords]"></v-text-field>
+                  </v-flex>
+                </v-layout>
+                <v-layout row>
+                  <v-flex xs12>
+                    <v-btn type="submit">Sign up</v-btn>
+                  </v-flex>
+                </v-layout>
+              </form>
+            </v-container>
+          </v-card-text>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        email:'',
+        password:'',
+        ConfirmPassword:''
+      }
+    },
+    computed: {
+      comparePasswords () {
+        return this.password !== this.ConfirmPassword ? 'Passwords do not match' : ''
+      }
+    },
+    methods: {
+      onSignup () {
+        console.log({email: this.email, password: this.password, ConfirmPassword: this.ConfirmPassword})
+      }
+    }
+  }
+</script>
