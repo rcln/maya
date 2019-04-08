03b3e42800e3ac3a235cd83c758b49ed7a88ee79
marwaLabiodh <47218871+marwaLabiodh@users.noreply.github.com>
2019-04-08 18:24:36 +0200

    Add files via upload

---
+++ Back-End/controllers/corpus-controller.js
@@ -0,0 +1,36 @@
+var express=require("express");
+var connection = require('./../config');
+
+
+
+
+
+
+
+
+
+
+
+/*module.exports.corpus=function(req,res){
+    var corpus={
+        "name_corpus":req.body.name_corpus,
+        "type_corpus":req.body.type_corpus,
+        
+    }
+
+
+    connection.query('INSERT INTO corpus SET ?',corpus, function (error, results, fields) {
+    	if(error){
+
+    	}
+    	else{
+
+    	}
+
+    })
+}*/
+
+
+
+
+
