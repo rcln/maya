44ed348070e58a99c22c97395e6623f0856f7b1d
Heba kaddouh <38936332+Hebakaddouh@users.noreply.github.com>
2019-05-12 18:51:03 +0200

    Add files via upload

--- Front-end/src/components/Import/TextReader.vue
+++ Front-end/src/components/Import/TextReader.vue
@@ -12,8 +12,18 @@ export default {
       const file = ev.target.files[0];
       const reader = new FileReader();
 
-      reader.onload = e => this.$emit("load", e.target.result);
-      reader.readAsText(file);
+      var output = [];
+      reader.onload = function(e) {
+        var text = e.target.result;
+        var lines = text.split(/[\r\n]+/g);
+
+        for(var i = 0; i < lines.length; i++) {
+          lines.forEach(function(line) {
+
+          });
+        }
+      }
+      //reader.readAsText(file);
     }
   }
 };
